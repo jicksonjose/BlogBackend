07e8f8143425e9666a054aebbbdc88262a7fb510
Jickson007 <Jicksonjose13@gmail.com>
2023-11-18 00:19:42 +0530

    create userapp , conf models, serializer, views

--- userapp/models.py
+++ userapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class UserModel(models.Model):
+    name=models.CharField(max_length=100,default="")
+    profileimg=models.CharField(max_length=100,default="")
+    email=models.CharField(max_length=100,default="")
+    password=models.CharField(max_length=100,default="")
+    
